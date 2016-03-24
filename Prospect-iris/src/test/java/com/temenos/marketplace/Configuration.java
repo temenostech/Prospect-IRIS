392a0ca7019cdf9211c90ff8501afe0cd0706193
Manoj Jangid <mjangid@temenos.com>
2016-03-24 11:33:45 +0000

    Fixed POM and added some integration test for fetching the events

---
+++ Prospect-iris/src/test/java/com/temenos/marketplace/Configuration.java
@@ -0,0 +1,17 @@
+package com.temenos.marketplace;
+
+/**
+ * TODO: Document me!
+ *
+ * @author mjangid
+ *
+ */
+public class Configuration {
+    public static String DATA_SERVICE_URL = "http://13.80.22.62:9007/MarketPlaceDemo-iris/MarketPlaceDemo.svc/GB0010001/";
+    
+    public static String INPUTTER_USER_NAME = "INPUTT";
+    public static String INPUTTER_PASSWORD = "123456";
+    
+    public static String AUTHORISER_USER_NAME = "SSOUSER1";
+    public static String AUTHORISER_PASSWORD = "123456";
+}
